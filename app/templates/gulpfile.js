7dc433d8ba6adcec27ff06b35cc9a7d11f835b00
Joel Rodrigues <19577724+joelfmrodrigues@users.noreply.github.com>
2020-09-03 23:13:22 +0100

    exclude condition for bundle analyzer

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -38,7 +38,6 @@ const path = require('path');
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 const gulpSequence = require('gulp-sequence');
-const fs = require("fs");
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
@@ -53,34 +52,35 @@ gulp.task('dev', gulpSequence('clean', 'bundle', 'package-solution'));
  * Webpack Bundle Anlayzer
  * Reference and gulp task
  */
-if (process.argv.indexOf('--analyze') !== -1 ||
-  process.argv.indexOf('dist') !== -1 ||
-  process.argv.indexOf('dev') !== -1) {
+// if (process.argv.indexOf('--analyze') !== -1 ||
+//   process.argv.indexOf('dist') !== -1 ||
+//   process.argv.indexOf('dev') !== -1) {
 
-  const bundleAnalyzer = require('webpack-bundle-analyzer');
+const fs = require("fs");
+const bundleAnalyzer = require('webpack-bundle-analyzer');
 
-  build.configureWebpack.mergeConfig({
+build.configureWebpack.mergeConfig({
 
-    additionalConfiguration: (generatedConfiguration) => {
-      <% if(spfxFastServe) {%>
-        fs.writeFileSync("./temp/_webpack_config.json", JSON.stringify(generatedConfiguration, null, 2));
-      <% }; %>
-      const lastDirName = path.basename(__dirname);
-      const dropPath = path.join(__dirname, 'temp', 'stats');
-      generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: 'static',
-        reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
-        generateStatsFile: true,
-        statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
-        logLevel: 'error'
-      }));
+additionalConfiguration: (generatedConfiguration) => {
+    <% if(spfxFastServe) {%>
+    fs.writeFileSync("./temp/_webpack_config.json", JSON.stringify(generatedConfiguration, null, 2));
+    <% }; %>
+    const lastDirName = path.basename(__dirname);
+    const dropPath = path.join(__dirname, 'temp', 'stats');
+    generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+    openAnalyzer: false,
+    analyzerMode: 'static',
+    reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+    generateStatsFile: true,
+    statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+    logLevel: 'error'
+    }));
 
-      return generatedConfiguration;
-    }
-
-  });
+    return generatedConfiguration;
 }
+
+});
+// }
 <% }; %>
 <% if(stylelint) {%>
 /**
